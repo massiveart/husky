fa721b0bc1baa234eab98be79ab7ecf5d5115cf6
Thomas Schedler <thomas@chirimoya.at>
2013-07-19 12:55:51 +0200

    update ui.data-grid tests

--- tests/modules/ui.data-grid.js
+++ tests/modules/ui.data-grid.js
@@ -1,11 +1,24 @@
 Husky.DEBUG = true;
 
 describe("Husky.UI.DataGrid", function() {
-    var $dataGrid;
+    var $dataGridContainer, server;
 
     beforeEach(function() {
-        $dataGrid = $('<div id="data-grid"/>');
-        $('body').append($dataGrid);
+        $dataGridContainer = $('<div id="js-data-grid"/>');
+        $('body').append($dataGridContainer);
+
+        server = sinon.fakeServer.create();
+
+        server.respondWith('GET', '/contacts?pageSize=4', [200, { 'Content-Type': 'application/json' },
+            '{"pageSize": "4", "page": "1", "total": "56", "head": [{ "content": "Head 1", "width": "100" }, { "content": "Head 2" }, { "content": "Head 3" }], "rows": [{ "id": "1", "columns": [{ "content": "Hallo 1" }, { "content": "Hallo 2" }, { "content": "Hallo 3" }] }, { "id": "2", "columns": [{ "content": "Hallo 1" }, { "content": "Hallo 2" }, { "content": "Hallo 3" }] }, { "id": "3", "columns": [{ "content": "Hallo 1" }, { "content": "Hallo 2" }, { "content": "Hallo 3" }] }, { "id": "4", "columns": [{ "content": "Hallo 1" }, { "content": "Hallo 2" }, { "content": "Hallo 3" }] }] }'
+        ]);
+
+        server.respondWith('GET', '/contacts?pageSize=4&page=2', [200, { 'Content-Type': 'application/json' },
+            '{"pageSize": "4", "page": "2", "total": "56", "head": [{ "content": "Head 1", "width": "100" }, { "content": "Head 2" }, { "content": "Head 3" }], "rows": [{ "id": "1", "columns": [{ "content": "Tschau" }, { "content": "Hallo 2" }, { "content": "Hallo 3" }] }, { "id": "2", "columns": [{ "content": "Hallo 1" }, { "content": "Hallo 2" }, { "content": "Hallo 3" }] }, { "id": "3", "columns": [{ "content": "Hallo 1" }, { "content": "Hallo 2" }, { "content": "Hallo 3" }] }, { "id": "4", "columns": [{ "content": "Hallo 1" }, { "content": "Hallo 2" }, { "content": "Hallo 3" }] }] }'
+        ]);
+
+        server.respond();
+
     });
 
     afterEach(function() {
@@ -13,12 +26,17 @@ describe("Husky.UI.DataGrid", function() {
     });
 
     it("Create instance", function() {
-        var dataGrid = $($dataGrid).huskyDataGrid({
-            url: '/data'
+
+        var $dataGrid = $('#js-data-grid').huskyDataGrid({
+            url: '/contacts',
+            pagination: true,
+            showPages: 6,
+            pageSize: 4,
+            selectItems: true
         });
 
-        console.log(dataGrid);
+        console.log($dataGrid.data('Husky.Ui.DataGrid'));
 
-        expect(dataGrid.data.length).toEqual(10);
+        // expect($dataGrid.data('Husky.Ui.DataGrid').data.length).toEqual(10);
     });
 });
