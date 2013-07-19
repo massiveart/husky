5ce583d0a98b63101feedb0d8581f151f83b7cc8
Thomas Schedler <thomas@chirimoya.at>
2013-07-19 13:09:19 +0200

    update ui.data-grid tests

--- tests/modules/ui.data-grid.js
+++ tests/modules/ui.data-grid.js
@@ -10,33 +10,37 @@ describe("Husky.UI.DataGrid", function() {
         server = sinon.fakeServer.create();
 
         server.respondWith('GET', '/contacts?pageSize=4', [200, { 'Content-Type': 'application/json' },
-            '{"pageSize": "4", "page": "1", "total": "56", "head": [{ "content": "Head 1", "width": "100" }, { "content": "Head 2" }, { "content": "Head 3" }], "rows": [{ "id": "1", "columns": [{ "content": "Hallo 1" }, { "content": "Hallo 2" }, { "content": "Hallo 3" }] }, { "id": "2", "columns": [{ "content": "Hallo 1" }, { "content": "Hallo 2" }, { "content": "Hallo 3" }] }, { "id": "3", "columns": [{ "content": "Hallo 1" }, { "content": "Hallo 2" }, { "content": "Hallo 3" }] }, { "id": "4", "columns": [{ "content": "Hallo 1" }, { "content": "Hallo 2" }, { "content": "Hallo 3" }] }] }'
+            '{"pageSize": "4", "page": "1", "total": 56, "head": [{ "content": "Head 1", "width": "100" }, { "content": "Head 2" }, { "content": "Head 3" }], "rows": [{ "id": "1", "columns": [{ "content": "Hallo 1" }, { "content": "Hallo 2" }, { "content": "Hallo 3" }] }, { "id": "2", "columns": [{ "content": "Hallo 1" }, { "content": "Hallo 2" }, { "content": "Hallo 3" }] }, { "id": "3", "columns": [{ "content": "Hallo 1" }, { "content": "Hallo 2" }, { "content": "Hallo 3" }] }, { "id": "4", "columns": [{ "content": "Hallo 1" }, { "content": "Hallo 2" }, { "content": "Hallo 3" }] }] }'
         ]);
 
         server.respondWith('GET', '/contacts?pageSize=4&page=2', [200, { 'Content-Type': 'application/json' },
-            '{"pageSize": "4", "page": "2", "total": "56", "head": [{ "content": "Head 1", "width": "100" }, { "content": "Head 2" }, { "content": "Head 3" }], "rows": [{ "id": "1", "columns": [{ "content": "Tschau" }, { "content": "Hallo 2" }, { "content": "Hallo 3" }] }, { "id": "2", "columns": [{ "content": "Hallo 1" }, { "content": "Hallo 2" }, { "content": "Hallo 3" }] }, { "id": "3", "columns": [{ "content": "Hallo 1" }, { "content": "Hallo 2" }, { "content": "Hallo 3" }] }, { "id": "4", "columns": [{ "content": "Hallo 1" }, { "content": "Hallo 2" }, { "content": "Hallo 3" }] }] }'
+            '{"pageSize": "4", "page": "2", "total": 56, "head": [{ "content": "Head 1", "width": "100" }, { "content": "Head 2" }, { "content": "Head 3" }], "rows": [{ "id": "1", "columns": [{ "content": "Tschau" }, { "content": "Hallo 2" }, { "content": "Hallo 3" }] }, { "id": "2", "columns": [{ "content": "Hallo 1" }, { "content": "Hallo 2" }, { "content": "Hallo 3" }] }, { "id": "3", "columns": [{ "content": "Hallo 1" }, { "content": "Hallo 2" }, { "content": "Hallo 3" }] }, { "id": "4", "columns": [{ "content": "Hallo 1" }, { "content": "Hallo 2" }, { "content": "Hallo 3" }] }] }'
         ]);
-
-        server.respond();
 
     });
 
     afterEach(function() {
-
+        server.restore();
     });
 
     it("Create instance", function() {
 
-        var $dataGrid = $('#js-data-grid').huskyDataGrid({
-            url: '/contacts',
-            pagination: true,
-            showPages: 6,
-            pageSize: 4,
-            selectItems: true
-        });
+        try {
+            var $dataGrid = $('#js-data-grid').huskyDataGrid({
+                url: '/contacts',
+                pagination: true,
+                showPages: 6,
+                pageSize: 4,
+                selectItems: true
+            });
 
-        console.log($dataGrid.data('Husky.Ui.DataGrid'));
+            server.respond();
 
-        // expect($dataGrid.data('Husky.Ui.DataGrid').data.length).toEqual(10);
+        } catch (e) {
+            // TODO fail('DataGrid instantiation');
+        }
+
+        expect(typeof $dataGrid.data('Husky.Ui.DataGrid')).toEqual('object')
+        expect($dataGrid.data('Husky.Ui.DataGrid').data.total).toEqual(56);
     });
 });
