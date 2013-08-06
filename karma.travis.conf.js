c56b33f8f46de3d3b56ab5a19c7536de9bd592b3
Thomas Schedler <thomas@chirimoya.at>
2013-08-06 16:59:52 +0200

    use PhantomJS for travis ci

--- karma.travis.conf.js
+++ karma.travis.conf.js
@@ -55,7 +55,7 @@ logLevel = LOG_INFO;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
-browsers = ['Chrome'];
+browsers = ['PhantomJS'];
 
 
 // If browser does not capture in given timeout [ms], kill it
