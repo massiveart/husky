a00b729a4a53ebe1806d67ae04d39277c246a292
Johannes Wachter <johannes.wachter@massiveart.com>
2013-09-03 16:54:56 +0200

    runed grunt build

--- assets/js/husky/dist/husky.js
+++ assets/js/husky/dist/husky.js
@@ -236,6 +236,12 @@ function typeOf(value) {
 
     var moduleName = 'Husky.Ui.AutoComplete';
 
+    var data = [];
+
+    var successClass = 'husky-auto-complete-success';
+    var failClass = 'husky-auto-complete-fail';
+    var loadingClass = 'husky-auto-complete-loading';
+
     Husky.Ui.AutoComplete = function(element, options) {
         this.name = moduleName;
 
@@ -299,7 +305,7 @@ function typeOf(value) {
             this.bindDOMEvents();
 
             if (this.options.value != null) {
-                this.successField();
+                this.successState();
             }
         },
 
@@ -349,7 +355,7 @@ function typeOf(value) {
 
                     } else {
                         // If dropdown not visible => search for given pattern
-                        this.noStateField();
+                        this.noState();
                         this.loadData(this.$valueField.val());
                     }
                 }.bind(this));
@@ -366,7 +372,7 @@ function typeOf(value) {
             Husky.DEBUG && console.log(this.name, 'inputChanged');
 
             // value is not success
-            this.noStateField();
+            this.noState();
 
             var val = this.$valueField.val();
             if (val.length >= this.options.minLength) {
@@ -378,17 +384,21 @@ function typeOf(value) {
         loadData: function(pattern) {
             var url = this.getUrl(pattern);
             Husky.DEBUG && console.log(this.name, 'load: ' + url);
+            this.loadingState();
 
             Husky.Util.ajax({
                 url: url,
                 success: function(response) {
                     Husky.DEBUG && console.log(this.name, 'load', 'success');
 
+                    this.noState();
+
                     // if only one result this is it, if no result hideDropDown, else generateDropDown
-                    if (response.total > 1) {
-                        this.generateDropDown(response.items);
-                    } else if (response.total == 1) {
-                        this.selectItem(response.items[0]);
+                    this.updateData(response.items);
+                    if (data.length > 1) {
+                        this.generateDropDown(data);
+                    } else if (data.length == 1) {
+                        this.selectItem(data[0]);
                     } else {
                         this.hideDropDown();
                     }
@@ -396,7 +406,7 @@ function typeOf(value) {
                 error: function() {
                     Husky.DEBUG && console.log(this.name, 'load', 'error');
 
-                    this.failField();
+                    this.failState();
                     this.hideDropDown();
                 }.bind(this)
             });
@@ -404,13 +414,34 @@ function typeOf(value) {
             this.trigger('auto-complete:loadData', null);
         },
 
+        // update global data array
+        updateData: function(newData) {
+            data = [];
+            newData.forEach(function(item) {
+                if (this.isVisible(item)) {
+                    data.push(item);
+                }
+            }.bind(this));
+        },
+
         // generate dropDown with given items
         generateDropDown: function(items) {
             this.clearDropDown();
             items.forEach(function(item) {
-                this.$dropDownList.append('<li data-id="' + item.id + '">' + item[this.options.valueName] + '</li>');
+                if (this.isVisible(item)) {
+                    this.$dropDownList.append('<li data-id="' + item.id + '">' + item[this.options.valueName] + '</li>');
+                }
             }.bind(this));
             this.showDropDown();
+        },
+
+        // is item visible (filter)
+        isVisible: function(item) {
+            var result = true;
+            this.options.excludeItems.forEach(function(testItem) {
+                if (item.id == testItem.id) result = false;
+            }.bind(this));
+            return result;
         },
 
         // clear childs of list
@@ -432,27 +463,38 @@ function typeOf(value) {
             this.$dropDown.hide();
         },
 
-        // set class success to field
-        successField: function() {
+        // set class success to container
+        successState: function() {
             Husky.DEBUG && console.log(this.name, 'set success');
             this.clearDropDown();
-            this.$valueField.removeClass('fail');
-            this.$valueField.addClass('success');
+            this.$originalElement.removeClass(failClass);
+            this.$originalElement.removeClass(loadingClass);
+            this.$originalElement.addClass(successClass);
         },
 
-        // remove class success and fail of field
-        noStateField: function() {
+        // remove class success, fail and loading of container
+        noState: function() {
             Husky.DEBUG && console.log(this.name, 'remove success and fail');
             this.$valueField.data('');
-            this.$valueField.removeClass('success');
-            this.$valueField.removeClass('fail');
+            this.$originalElement.removeClass(failClass);
+            this.$originalElement.removeClass(loadingClass);
+            this.$originalElement.removeClass(successClass);
         },
 
-        // add class fail to field
-        failField: function() {
+        // add class fail to container
+        failState: function() {
             Husky.DEBUG && console.log(this.name, 'set fail');
-            this.$valueField.removeClass('success');
-            this.$valueField.addClass('fail');
+            this.$originalElement.addClass(failClass);
+            this.$originalElement.removeClass(loadingClass);
+            this.$originalElement.removeClass(successClass);
+        },
+
+        // add class loading to container
+        loadingState: function() {
+            Husky.DEBUG && console.log(this.name, 'set loading');
+            this.$originalElement.removeClass(failClass);
+            this.$originalElement.addClass(loadingClass);
+            this.$originalElement.removeClass(successClass);
         },
 
         // handle key down
@@ -524,7 +566,7 @@ function typeOf(value) {
             this.$valueField.val(item[this.options.valueName]);
 
             this.hideDropDown();
-            this.successField();
+            this.successState();
         }
     });
 
@@ -545,11 +587,12 @@ function typeOf(value) {
     };
 
     $.fn.huskyAutoComplete.defaults = {
-        url: '',
-        valueName: 'name',
-        minLength: 3,
-        keyControl: true,
-        value: null
+        url: '', // url to load data
+        valueName: 'name', // propertyName for value
+        minLength: 3, // min length for request
+        keyControl: true, // control with up/down key
+        value: null, // value to display at start
+        excludeItems: [] // items to filter
     };
 
 })(Husky.$, this, this.document);
@@ -1408,11 +1451,22 @@ function typeOf(value) {
             this.clearDropDown();
             if (items.length > 0) {
                 items.forEach(function(item) {
-                    this.$dropDownList.append('<li data-id="' + item.id + '">' + item[this.options.valueName] + '</li>');
+                    if (this.isVisible(item)) {
+                        this.$dropDownList.append('<li data-id="' + item.id + '">' + item[this.options.valueName] + '</li>');
+                    }
                 }.bind(this));
             } else {
                 this.$dropDownList.append('<li>No data received</li>');
             }
+        },
+
+        // is item visible (filter)
+        isVisible: function(item) {
+            var result = true;
+            this.options.excludeItems.forEach(function(testItem) {
+                if (item.id == testItem.id) result = false;
+            }.bind(this));
+            return result;
         },
 
         // clear childs of list
@@ -1465,7 +1519,8 @@ function typeOf(value) {
         data: [],    // data array
         trigger: '',  // trigger for click event
         valueName: 'name', // name of text property
-        setParentDropDown: false // set class dropdown for parent dom object
+        setParentDropDown: false, // set class dropdown for parent dom object
+        excludeItems: [] // items to filter
     };
 
 })(Husky.$, this, this.document);
