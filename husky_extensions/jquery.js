c1c87df15cff6d9653ff076b6e1750766c273a34
Erfan Ebrahimnia <erfan.ebrahimnia@massiveart.com>
2013-09-07 17:48:02 +0200

    updated jquery extension

--- husky_extensions/jquery.js
+++ husky_extensions/jquery.js
@@ -36,12 +36,32 @@
                 return $(target).removeClass(classes);
             };
 
+            app.core.dom.width = function(element) {
+                return $(element).width();
+            };
+
+            app.core.dom.height = function(element) {
+                return $(element).height();
+            };
+
             app.core.dom.remove = function(element) {
                 return $(element).remove();
             };
 
-            app.core.util.ajax = $.ajax;
+            app.core.dom.attr = function(element, attributes) {
+                attributes = attributes || {};
+                return $(element).remove();
+            };
 
+            app.core.dom.is = function(element, type) {
+                return $(element).is(type);
+            };
+
+            app.core.dom.onReady = function(callback) {
+                $(window).on('load', callback);
+            };
+
+            app.core.util.ajax = $.ajax;
         }
     });
 })();
