e70bef0ac18b938d772e306b00fa04d859cfac36
Johannes Wachter <johannes.wachter@massiveart.com>
2013-10-05 13:18:01 +0200

    added translate labels dropdown

--- assets/js/husky/dist/husky.js
+++ assets/js/husky/dist/husky.js
@@ -22672,7 +22672,8 @@ define('__component__$dropdown@husky',['jquery'], function($) {
             setParentDropDown: false, // set class dropdown for parent dom object
             excludeItems: [], // items to filter,
             instanceName: 'undefined',  // instance name
-            alignment: 'left'
+            alignment: 'left',  // alignment of the arrow and the box
+            translateLabels: true   // translate labels withe globalize
         };
 
 
@@ -22832,7 +22833,11 @@ define('__component__$dropdown@husky',['jquery'], function($) {
             if (items.length > 0) {
                 items.forEach(function(item) {
                     if (this.isVisible(item)) {
-                        this.$dropDownList.append('<li data-id="' + item.id + '">' + item[this.options.valueName] + '</li>');
+                        var label = item[this.options.valueName];
+                        if (this.options.translateLabels) {
+                            label = this.sandbox.translate(label);
+                        }
+                        this.$dropDownList.append('<li data-id="' + item.id + '">' + label + '</li>');
                     }
                 }.bind(this));
             } else {
