034e7f0771d0d606a3193a68b45490731461d871
mob8607 <mathias.ober@massiveart.com>
2013-10-14 10:50:46 +0200

    updated button test

--- tests/components/ButtonSpec.js
+++ tests/components/ButtonSpec.js
@@ -24,8 +24,7 @@ define(['husky'], function(husky) {
 
             waitsFor(function() {
                 return flag;
-//                return $('.icon-btn').size() === 1;
-            }, 'load icon', 500);
+            }, 'load component', 500);
 
             runs(function() {
 
@@ -33,15 +32,32 @@ define(['husky'], function(husky) {
         });
 
         afterEach(function() {
-//            app.stop();
-//            $('.icon-btn').remove();
         });
 
         it('should be inserted into the dom', function() {
             expect($('.icon-btn').size()).toBe(1);
         });
 
-        it('has icon ok-circle', function() {
+        it('should change status to be disabled', function() {
+            var flag = false;
+            runs(function() {
+                app.sandbox.emit('husky.button.instance.state', 'disable');
+
+                _.delay(function() {
+                    flag = true;
+                }, 300);
+            });
+
+            waitsFor(function() {
+                return flag;
+            }, 'load component', 500);
+
+            runs(function() {
+                expect($('.icon-btn.disable').size()).toBe(1);
+            });
+        });
+
+        it('should change icon to ok-circle', function() {
             var flag = false;
             runs(function() {
                 app.sandbox.emit('husky.button.instance.set-content', 'myText', 'circle-ok');
@@ -57,25 +73,6 @@ define(['husky'], function(husky) {
 
             runs(function() {
                 expect($('.icon-btn').find('.loading-content').find('.icon-circle-ok').size()).toBe(1);
-            });
-        });
-
-        it('can be disabled', function() {
-            var flag = false;
-            runs(function() {
-                app.sandbox.emit('husky.button.instance.state', 'disable');
-
-                _.delay(function() {
-                    flag = true;
-                }, 100);
-            });
-
-            waitsFor(function() {
-                return flag;
-            }, 'load component', 500);
-
-            runs(function() {
-                expect($('.icon-btn.disable').size()).toBe(1);
             });
         });
     });
