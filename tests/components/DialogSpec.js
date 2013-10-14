723bf796906a98d86c1322420e84e5c29704b670
Erfan Ebrahimnia <erfan.ebrahimnia@massiveart.com>
2013-10-14 17:46:18 +0200

    init dialog test

---
+++ tests/components/DialogSpec.js
@@ -0,0 +1,44 @@
+define(['husky'], function(husky) {
+
+    'use strict';
+
+    var app;
+
+    describe('Dialog', function() {
+
+        beforeEach(function() {
+            $('body').append($('<div/>', {
+                id: 'dialog'
+            }));
+
+            app.start().then(function() {
+                app.sandbox.start([
+                    {
+                        name: 'dialog@husky',
+                        options: {
+                            el: '#dialog',
+                            data: {
+                                content: {
+                                    title: "This is the headline!",
+                                    content: "This is the content"
+                                },
+                                footer: {
+                                    buttonCancelText: "Cancel",
+                                    buttonSubmitText: "Save"
+                                }
+                            }
+                        }
+                    }
+                ]);
+            });
+        });
+
+        afterEach(function() {
+            // TODO
+        });
+
+        it('should be initialized', function() {
+            expect($('.dialog-container').size()).toBe(1);
+        });
+    });
+});
