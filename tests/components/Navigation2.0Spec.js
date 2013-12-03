591fef620b6ffd063644a900c3d09ffc7e23c325
Michael Zangerle <michael.zangerle@massiveart.com>
2013-12-03 13:17:33 +0100

    worked on navigation tests

--- tests/components/Navigation2.0Spec.js
+++ tests/components/Navigation2.0Spec.js
@@ -230,45 +230,50 @@ define(['husky'], function(husky) {
 
         });
 
-        // TODO also for sections?
+//        // TODO also for sections?
 //        /**
 //             check if class  is-expanded of navigation item is removed after click
 //             and subnav is hidden
 //         */
-//        it('should have class is-expanded removed and submenu hidden', function() {
+//        it('item should have class is-expanded removed and submenu hidden', function() {
 //
 //            var flag1 = false,
 //                flag2 = false,
 //                $toggleItem = $('.navigation-items-toggle').eq(1);
 //
 //            runs(function() {
-//                _.delay(function(){
-//                    $toggleItem.trigger('click');
+////                _.delay(function(){
+//                    $toggleItem.click();
 //                    flag1 = true;
-//                },100);
+////                },100);
 //            });
-//
 //
 //            waitsFor(function() {
 //                return flag1;
-//            }, 'Element should have been clicked!', 200);
+//            }, 'Element should have been clicked!', 500);
+//
+////            runs(function() {
+////                console.log($toggleItem.parent().hasClass('is-expanded'));
+////                expect($toggleItem.parent().hasClass('is-expanded')).toBe(false);
+////            });
 //
 //            runs(function() {
-//                _.delay(function(){
-//                    $toggleItem.trigger('click');
+////                _.delay(function(){
+//                    $toggleItem.click();
 //                    flag2 = true;
-//                },100);
+////                },600);
 //            });
 //
 //            waitsFor(function() {
-//                return flag2;
-//            }, 'Element should have been clicked!', 200);
+//                return flag2 && flag1;
+//            }, 'Element should have been clicked!', 700);
 //
 //            runs(function() {
+//                console.log($toggleItem.parent().hasClass('is-expanded'));
 //                expect($toggleItem.parent().hasClass('is-expanded')).toBe(false);
 //            });
 //        });
-//
+
         /**
          check if class is-selected is set on navigation-sub-item after click
          */
@@ -339,6 +344,7 @@ define(['husky'], function(husky) {
             });
         });
 
+        // TODO not implemented
 //        /**
 //            check if initialized event is emitted
 //         */
