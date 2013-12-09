e2f7e9f3e937129839f121acef1a3006354a4f51
Michael Zangerle <michael.zangerle@massiveart.com>
2013-12-09 14:41:28 +0100

    worked on column navigation (css, basic rendering)

--- demos/column-navigation/app_columnNavigation.js
+++ demos/column-navigation/app_columnNavigation.js
@@ -12,33 +12,108 @@ require(['lib/husky'], function (Husky) {
 
     fakeServer = sinon.fakeServer.create();
 
-    fakeServer.respondWith('GET', '/admin/api/contacts?flat=true&page=1&pageSize=4&sortBy=content1&sortOrder=asc', [200, { 'Content-Type': 'application/json' },
-        '{' +
-            '"_links":' +
-            '{' +
-            '"first":"/admin/api/contacts?flat=true&page=1&pageSize=4",' +
-            '"last": "/admin/api/contacts?flat=true&page=3&pageSize=4",' +
-            '"self":"/admin/api/contacts?flat=true&pageSize=4&sortBy=content1&sortOrder=asc",' +
-            '"next":"/admin/api/contacts?flat=true&page=2&pageSize=4",' +
-            '"pagination": "/admin/api/contacts?flat=true&page={page}&pageSize=4",'+
-
-            '"sortable": {' +
-            '"content1" : "/admin/api/contacts?flat=true&page=1&pageSize=4&sortBy=content1&sortOrder={sortOrder}",' +
-            '"content2" : "/admin/api/contacts?flat=true&page=1&pageSize=4&sortBy=content2&sortOrder={sortOrder}"' +
-            '}'+
-            '},' +
-            '"_embedded":'+
-            '['+
-            '{ "id": "1", "content1": "A Hallo 1.1", "content2": "C Hallo 1.2", "content3": { "thumb": "http://placehold.it/24x24", "alt": "lorempixel" } }, ' +
-            '{ "id": "2", "content1": "B Hallo 1.1", "content2": "B Hallo 1.2", "content3": { "thumb": "http://placehold.it/24x24", "alt": "lorempixel" } }, ' +
-            '{ "id": "3", "content1": "C Hallo 1.1", "content2": "D Hallo 1.2", "content3": { "thumb": "http://placehold.it/24x24", "alt": "lorempixel" } }, ' +
-            '{ "id": "4", "content1": "D Hallo 1.1", "content2": "A Hallo 1.2", "content3": { "thumb": "http://placehold.it/24x24", "alt": "lorempixel" } }'+
+    fakeServer.respondWith('GET', '/nodes?depth=1', [200, { 'Content-Type': 'application/json' },
+        '{'+
+            '"_links" : {'+
+                '"self": "/nodes/1",'+
+                '"children" : "/nodes?parent=1&depth=1"'+
+            '},'+
+            '"_embedded": ['+
+                '{'+
+                    '"title": "Page 121",'+
+                    '"id": "121",'+
+                    '"hasSub": true,'+
+                    '"_links" : {'+
+                    '"self" : "/nodes/121",'+
+                    '"children" : "/nodes?parent=121&depth=1"'+
+                    '},'+
+                    '"_embedded": []'+
+                '},'+
+                '{'+
+                    '"title": "Page 122",'+
+                    '"id": "122",'+
+                    '"hasSub": true,'+
+                    '"_links" : {'+
+                        '"self" : "/nodes/122",'+
+                        '"children" : "/nodes?parent=122&depth=1"'+
+                    '},'+
+                    '"_embedded": []'+
+                '},'+
+                '{'+
+                    '"title": "Page 123",'+
+                    '"id": "123",'+
+                    '"hasSub": true,'+
+                    '"_links" : {'+
+                        '"self" : "/nodes/123",'+
+                        '"children" : "/nodes?parent=123&depth=1"'+
+                    '},'+
+                    '"_embedded": []'+
+                '},'+
+                '{'+
+                    '"title": "Page 124",'+
+                    '"id": "124",'+
+                    '"hasSub": true,'+
+                    '"_links" : {'+
+                        '"self" : "/nodes/124",'+
+                        '"children" : "/nodes?parent=124&depth=1"'+
+                    '},'+
+                    '"_embedded": []'+
+                '},'+
+                '{'+
+                    '"title": "Page 125",'+
+                    '"id": "125",'+
+                    '"hasSub": true,'+
+                    '"_links" : {'+
+                        '"self" : "/nodes/125",'+
+                        '"children" : "/nodes?parent=125&depth=1"'+
+                    '},'+
+                    '"_embedded": []'+
+                '},'+
+                '{'+
+                    '"title": "Page 126",'+
+                    '"id": "126",'+
+                    '"hasSub": true,'+
+                    '"_links" : {'+
+                        '"self" : "/nodes/126",'+
+                        '"children" : "/nodes?parent=126&depth=1"'+
+                    '},'+
+                    '"_embedded": []'+
+                '},'+
+                '{'+
+                    '"title": "Page 127",'+
+                    '"id": "127",'+
+                    '"hasSub": true,'+
+                    '"_links" : {'+
+                        '"self" : "/nodes/127",'+
+                        '"children" : "/nodes?parent=127&depth=1"'+
+                    '},'+
+                    '"_embedded": []'+
+                '},'+
+                '{'+
+                    '"title": "Page 128",'+
+                    '"id": "128",'+
+                    '"hasSub": true,'+
+                    '"_links" : {'+
+                        '"self" : "/nodes/128",'+
+                        '"children" : "/nodes?parent=128&depth=1"'+
+                    '},'+
+                    '"_embedded": []'+
+                '},'+
+                '{'+
+                    '"title": "Page 129",'+
+                    '"id": "129",'+
+                    '"hasSub": true,'+
+                    '"_links" : {'+
+                        '"self" : "/nodes/129",'+
+                        '"children" : "/nodes?parent=129&depth=1"'+
+                    '},'+
+                    '"_embedded": []'+
+                '}'+
             '],'+
-            '"total":12,'+
-            '"pages": 3,' +
-            '"page": 1,' +
-            '"pageSize": 4' +
-            '}'
+            '"title": "Root",'+
+            '"id": "1",'+
+            '"hasSub": true'+
+        '}'
     ]);
 
 
@@ -50,15 +125,16 @@ require(['lib/husky'], function (Husky) {
             {
                 name: 'column-navigation@husky',
                 options: {
-                    el: '#column-navigation'
+                    el: '#column-navigation',
+                    url: '/nodes?depth=1'
                 }
             }
         ]).then(function () {
             app.logger.log('Aura started...');
 
-//            _.delay(function () {
-//                fakeServer.respond();
-//            }, 500);
+            _.delay(function () {
+                fakeServer.respond();
+            }, 500);
 
         });
 });
--- husky_components/column-navigation/main.js
+++ husky_components/column-navigation/main.js
@@ -11,7 +11,7 @@
  *
  * Emits:
  *  husky.column.navigation.initialized
- *  husky.column.navigation.loaded
+ *  husky.column.navigation.data.loaded
  *  husky.column.navigation.selected
  *
  * Listens:
@@ -21,15 +21,71 @@ define([], function() {
 
     'use strict';
 
+    var defaults = {
+        wrapper: {
+            height: "300px"
+        },
+        url: null
+    };
+
     return {
 
         initialize: function() {
 
-            this.sandbox.logger.log("hello world!");
+            this.sandbox.logger.log("column navigation initializing ...");
+            this.options = this.sandbox.util.extend(true, {}, defaults, this.options);
 
+            this.render();
+            this.load();
+
+            this.sandbox.emit('husky.column.navigation.initialized');
         },
 
+        /**
+         * Renders basic structure (wrapper) of column navigation
+         */
         render: function() {
+            var $wrapper;
+
+            $wrapper = this.sandbox.dom.$('<div/>');
+            this.sandbox.dom.addClass($wrapper, 'column-navigation');
+            this.sandbox.dom.css($wrapper, 'height', this.options.wrapper.height);
+
+            this.$element = this.sandbox.dom.$(this.options.el);
+            this.sandbox.dom.append(this.$element, $wrapper);
+
+            // TODO
+            // add and settings button
+        },
+
+        /**
+         * Loads data from a specific url
+         */
+        load: function() {
+
+            this.sandbox.util.ajax({
+
+                url: this.options.url,
+
+                error: function(jqXHR, textStatus, errorThrown) {
+                    this.sandbox.logger.log("An error occured while fetching data from: " + this.options.url);
+                    this.sandbox.logger.log("errorthrown",errorThrown.message);
+                }.bind(this),
+
+                success: function(response) {
+
+                    this.data = {};
+                    this.data.links = response._links;
+                    this.data.embedded = response._embedded;
+                    this.data.title = response.title;
+                    this.data.id = response.id;
+                    this.data.hasSub = response.hasSub;
+
+                    this.sandbox.emit('husky.column.navigation.data.loaded');
+
+                }.bind(this)
+            });
+
 
         }
     };
