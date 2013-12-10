2c7a07473804a4d8c759fb5081d682085225b693
Johannes Wachter <johannes.wachter@massiveart.com>
2013-12-10 15:58:15 +0100

    reviewed code

--- husky_components/toolbar/main.js
+++ husky_components/toolbar/main.js
@@ -21,32 +21,36 @@ define(function() {
     'use strict';
 
     var defaults = {
-        url: null,
-        data: [],
-        instanceName: '',
-        hasSearch: false,
-        appearance: 'large'
-    },
+            url: null,
+            data: [],
+            instanceName: '',
+            hasSearch: false,
+            appearance: 'large'
+        },
 
-    selectItem = function(event) {
-        event.preventDefault();
+        selectItem = function(event) {
+            event.preventDefault();
 
-        triggerSelectEvent.call(this, this.items[this.sandbox.dom.data(event.currentTarget, 'id')]);
-    },
+            triggerSelectEvent.call(this, this.items[this.sandbox.dom.data(event.currentTarget, 'id')]);
+        },
 
-    triggerSelectEvent = function(item) {
-        // if callback is set call it, else trigger event
-        if (item.callback) {
-            item.callback();
-        } else {
-            var instanceName = this.options.instanceName ? this.options.instanceName+'.':'';
-            this.sandbox.emit('husky.toolbar.'+instanceName+'item.select', item);
-        }
-    },
+        triggerSelectEvent = function(item) {
+            // if callback is set call it, else trigger event
+            if (item.callback) {
+                item.callback();
+            } else {
+                this.sandbox.emit(createEventString.call(this, 'item.select'), item);
+            }
+        },
 
-    bindDOMEvents = function() {
-        this.sandbox.dom.on(this.options.el,'click',selectItem.bind(this),'button:not(:disabled)');
-    };
+        bindDOMEvents = function() {
+            this.sandbox.dom.on(this.options.el, 'click', selectItem.bind(this), 'button:not(:disabled)');
+        },
+
+        createEventString = function(ending) {
+            var instanceName = this.options.instanceName ? this.options.instanceName + '.' : '';
+            return 'husky.toolbar.' + instanceName + ending;
+        };
 
     return {
 
@@ -87,14 +91,13 @@ define(function() {
             // save item groups in array
             this.itemGroup = [];
 
-
             // create all elements
             this.sandbox.util.foreach(data, function(item) {
 
                 // if item has no id, generate random id
                 if (!item.id) {
                     item.id = 'xxxxyxxx'.replace(/[xy]/g, function(c) {
-                        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+                        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
                         return v.toString(16);
                     });
                 }
@@ -103,14 +106,14 @@ define(function() {
                 // create classes array
                 classArray = [];
                 if (item.icon) {
-                    classArray.push('icon-'+item.icon);
+                    classArray.push('icon-' + item.icon);
                 }
 
                 // check if item is in a group
                 if (!!item.group) {
                     // create group if not created yet
                     if (!this.itemGroup[item.group]) {
-                        this.itemGroup[item.group] = this.sandbox.dom.createElement('<div id="'+item.group+'" class="btn-group" />');
+                        this.itemGroup[item.group] = this.sandbox.dom.createElement('<div id="' + item.group + '" class="btn-group" />');
                         this.sandbox.dom.append($container, this.itemGroup[item.group]);
                         this.sandbox.dom.addClass(this.itemGroup[item.group], this.options.appearance);
                     }
@@ -120,33 +123,30 @@ define(function() {
                     classArray.push(this.options.appearance);
                 }
 
-
-
-
                 if (!!item.class) {
                     classArray.push(item.class);
                 }
 
-                disabledString ='';
+                disabledString = '';
                 if (item.disabled) {
                     disabledString = 'disabled';
                 }
                 // create button
-                this.sandbox.dom.append(addTo, '<button data-id="'+item.id+'" class="'+classArray.join(' ')+'" title="'+item.title+'" '+disabledString+'/>');
+                this.sandbox.dom.append(addTo, '<button data-id="' + item.id + '" class="' + classArray.join(' ') + '" title="' + item.title + '" ' + disabledString + '/>');
 
             }.bind(this));
 
             // add search
             if (this.options.hasSearch) {
-                this.sandbox.dom.append($container, '<div id="'+this.options.instanceName+'-toolbar-search" class="toolbar-search" />');
+                this.sandbox.dom.append($container, '<div id="' + this.options.instanceName + '-toolbar-search" class="toolbar-search" />');
             }
             // start search component
             this.sandbox.start([
-                {name: 'search@husky', options: {el: '#'+this.options.instanceName+'-toolbar-search', instanceName: this.options.instanceName, appearance:'white small'}}
+                {name: 'search@husky', options: {el: '#' + this.options.instanceName + '-toolbar-search', instanceName: this.options.instanceName, appearance: 'white small'}}
             ]);
 
             // initialization finished
-            this.sandbox.emit('husky.tabs.initialized');
+            this.sandbox.emit(createEventString.call(this, 'initialized'));
         }
     };
 
