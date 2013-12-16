25740ddf7c67c6ad92370fcc03315e371912fa6b
Johannes Wachter <johannes.wachter@massiveart.com>
2013-12-16 13:22:22 +0100

    changed event names

--- husky_components/edit-toolbar/main.js
+++ husky_components/edit-toolbar/main.js
@@ -304,7 +304,7 @@ define(function() {
 
         /** returns normalized event names */
         createEventName = function(postFix) {
-            return 'husky.edittoolbar.' + (this.options.instanceName ? this.options.instanceName + '.' : '') + postFix;
+            return 'husky.edit-toolbar.' + (this.options.instanceName ? this.options.instanceName + '.' : '') + postFix;
         };
 
     return {
