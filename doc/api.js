104ff336d8972df0aefe7955a8f3effbd3b90034
Johannes Wachter <johannes.wachter@massiveart.com>
2014-01-21 08:22:49 +0100

    review code

--- doc/api.js
+++ doc/api.js
@@ -2,6 +2,7 @@ YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
         "AutoCompleteList",
+        "CKEditor",
         "ColumnNavigation",
         "EditToolbar",
         "Loader",
