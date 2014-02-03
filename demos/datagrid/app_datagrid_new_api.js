1de5b05e59a9e16d49976a9968ecb9bd4978fc70
Michael Zangerle <michael.zangerle@massiveart.com>
2014-02-03 13:07:40 +0100

    disabled validation in demo

--- demos/datagrid/app_datagrid_new_api.js
+++ demos/datagrid/app_datagrid_new_api.js
@@ -421,7 +421,7 @@ require(['lib/husky'], function(Husky) {
                     removeRow: true,
                     pagination: true,
                     editable: true,
-                    validation:true,
+                    validation:false,
                     columns: [
                         {
                             content: 'Content 1',
