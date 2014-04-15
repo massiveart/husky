328ddfa1bd4bb5acb57ff65b1b580d7cd7ed47f5
marcelmoosbrugger <marcel.moosbrugger@massiveart.com>
2014-04-15 10:24:37 +0200

    stopped loader properly

--- husky_components/toolbar/main.js
+++ husky_components/toolbar/main.js
@@ -302,7 +302,7 @@ define(function() {
             if (item.loading) {
                 item.loading = false;
                 $itemLink = this.sandbox.dom.find('a', $item);
-                this.sandbox.dom.remove(this.sandbox.dom.find('.item-loader', $item));
+                this.sandbox.stop(this.sandbox.dom.find('.item-loader', $item));
                 this.sandbox.dom.show($itemLink);
             }
 
