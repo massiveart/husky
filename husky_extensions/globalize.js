dcf1ae3105a23536b4001e78a00fb72fe2a5209c
Johannes Wachter <johannes.wachter@massiveart.com>
2014-04-17 11:19:14 +0200

    implemented date parse and format

--- husky_extensions/globalize.js
+++ husky_extensions/globalize.js
@@ -42,6 +42,63 @@
                     return !!translation ? translation : key;
                 };
 
+                app.sandbox.date = {
+                    /**
+                     * returns formatted date string
+                     * @param {string|Date} date
+                     * @returns {string}
+                     */
+                    format: function(date) {
+                        if(typeof date === 'string'){
+                            date = this.parse(date);
+                        }
+                        return Globalize.format(date);
+                    },
+
+                    /**
+                     * parse ISO8601 string : string -> Date
+                     * Parse an ISO-8601 date, including possible timezone,
+                     * into a Javascript Date object.
+                     * (inspired by: http://stackoverflow.com/questions/439630/how-do-you-create-a-javascript-date-object-with-a-set-timezone-without-using-a-s)
+                     *
+                     * Test strings: parseISO8601String(x).toISOString()
+                     * "2013-01-31T12:34"              -> "2013-01-31T12:34:00.000Z"
+                     * "2013-01-31T12:34:56"           -> "2013-01-31T12:34:56.000Z"
+                     * "2013-01-31T12:34:56.78"        -> "2013-01-31T12:34:56.780Z"
+                     * "2013-01-31T12:34:56.78+0100"   -> "2013-01-31T11:34:56.780Z"
+                     * "2013-01-31T12:34:56.78+0530"   -> "2013-01-31T07:04:56.780Z"
+                     * "2013-01-31T12:34:56.78-0330"   -> "2013-01-31T16:04:56.780Z"
+                     * "2013-01-31T12:34:56-0330"      -> "2013-01-31T16:04:56.000Z"
+                     * "2013-01-31T12:34:56Z"          -> "2013-01-31T12:34:56.000Z"
+                     *
+                     * @param {string} dateString
+                     * @returns {Date}
+                     */
+                    parse: function(dateString) {
+                        var timebits = /^([0-9]{4})-([0-9]{2})-([0-9]{2})T([0-9]{2}):([0-9]{2})(?::([0-9]*)(\.[0-9]*)?)?(?:([+-])([0-9]{2})([0-9]{2}))?/,
+                            m = timebits.exec(dateString),
+                            resultDate;
+
+                        if (m) {
+                            var utcdate = Date.UTC(parseInt(m[1]),
+                                parseInt(m[2]) - 1, // months are zero-offset (!)
+                                parseInt(m[3]),
+                                parseInt(m[4]), parseInt(m[5]), // hh:mm
+                                (m[6] && parseInt(m[6]) || 0),  // optional seconds
+                                (m[7] && parseFloat(m[7]) * 1000) || 0); // optional fraction
+                            // utcdate is milliseconds since the epoch
+                            if (m[9] && m[10]) {
+                                var offsetMinutes = parseInt(m[9]) * 60 + parseInt(m[10]);
+                                utcdate += (m[8] === '+' ? -1 : +1) * offsetMinutes * 60000;
+                            }
+                            resultDate = new Date(utcdate);
+                        } else {
+                            resultDate = null;
+                        }
+                        return resultDate;
+                    }
+                };
+
                 /**
                  * function calls this.sandbox.translate for an array of keys and returns an array of translations
                  * @param array
