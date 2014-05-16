1df63b5700c6b6e6b04234f491f5f1ed347d7709
marcelmoosbrugger <marcel.moosbrugger@massiveart.com>
2014-05-16 07:34:09 +0200

    small bugfix

--- husky_components/datagrid/main.js
+++ husky_components/datagrid/main.js
@@ -619,7 +619,8 @@
              * @param options {Object} an object with options to merge with the current view options for the view
              */
             changeView: function(view, options) {
-                if (view !== this.viewId) {
+                // only change if view or options are different
+                if (view !== this.viewId || this.sandbox.util.compare(options, this.options.viewOptions[this.viewId])) {
                     this.destroy();
                     this.getViewDecorator(view);
                     this.extendViewOptions(options);
