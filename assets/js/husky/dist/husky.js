27171917334ab596bba63349e8cd5307512cf282
Michael Zangerle <michael.zangerle@massiveart.com>
2014-05-20 16:44:44 +0200

    added new build

--- assets/js/husky/dist/husky.js
+++ assets/js/husky/dist/husky.js
@@ -35259,11 +35259,11 @@ define('__component__$select@husky',[], function() {
         /**
          * Updates the dropdown list
          * @param data
-         * @param preselected
+         * @param preselected array of ids
          */
         updateDropdown: function(data, preselected){
 
-            this.options.preSelectedElements = preselected;
+            this.options.preSelectedElements = preselected.map(String);
             this.selectedElements = [];
             this.selectedElementsValues = [];
             this.sandbox.dom.empty(this.$list);
