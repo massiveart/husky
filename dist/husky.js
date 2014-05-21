99cd5ea319c840bdd12260a9287c6a570320eb43
marcelmoosbrugger <marcel.moosbrugger@massiveart.com>
2014-05-21 15:12:50 +0200

    automatically added build files

--- dist/husky.js
+++ dist/husky.js
@@ -39785,15 +39785,6 @@ define('__component__$dropzone@husky',[], function() {
         },
 
         /**
-         * raised after file got removed from the zone
-         * @event husky.dropzone.<instance-name>.file-remove
-         * @param {Object} the file
-         */
-            FILE_REMOVED = function() {
-            return createEventName.call(this, 'file-removed');
-        },
-
-        /**
          * raised after files got uploaded and faded out from the dropzone
          * @event husky.dropzone.<instance-name>.files-added
          * @param {Array} all newly added files
@@ -39951,8 +39942,6 @@ define('__component__$dropzone@husky',[], function() {
                         this.on('removedfile', function(file) {
                             if (typeof this.options.removeFileCallback === 'function') {
                                 this.options.removeFileCallback(file);
-                            } else {
-                                this.sandbox.emit(FILE_REMOVED.call(this), file);
                             }
                         }.bind(that));
 
@@ -39999,6 +39988,7 @@ define('__component__$dropzone@husky',[], function() {
         afterFadeOut: function() {
             this.sandbox.dom.removeClass(this.$dropzone, constants.droppedClass);
             this.sandbox.emit(FILES_ADDED.call(this), this.getResponseArray(this.dropzone.files));
+            this.dropzone.removeAllFiles();
         },
 
         /**
