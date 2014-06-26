ad4e550fef3a60c46f2c589bb34f3803e6a2816e
marcelmoosbrugger <marcel.moosbrugger@massiveart.com>
2014-06-26 16:34:45 +0200

    automatically added build files

--- assets/js/husky/dist/husky.js
+++ assets/js/husky/dist/husky.js
@@ -29932,7 +29932,7 @@ define('husky_components/datagrid/decorators/thumbnail-view',[],function() {
             this.data = data;
 
             this.renderThumbnails(this.data.embedded);
-
+            this.sandbox.dom.on('body', 'click.grid-thumbnails.' + this.datagrid.options.instanceName, this.unselectAll.bind(this));
             this.rendered = true;
         },
 
@@ -30015,7 +30015,7 @@ define('husky_components/datagrid/decorators/thumbnail-view',[],function() {
          * Destroys the view
          */
         destroy: function() {
-            this.sandbox.dom.off('body', 'click.grid-thumbnails');
+            this.sandbox.dom.off('body', 'click.grid-thumbnails.' + this.ddatagrid.options.instanceName);
             this.sandbox.dom.remove(this.$el);
         },
 
@@ -30038,8 +30038,6 @@ define('husky_components/datagrid/decorators/thumbnail-view',[],function() {
                 this.datagrid.emitItemClickedEvent.call(this.datagrid, id);
                 this.selectItem(id);
             }.bind(this));
-
-            this.sandbox.dom.on('body', 'click.grid-thumbnails', this.unselectAll.bind(this));
         },
 
         /**
@@ -30048,9 +30046,9 @@ define('husky_components/datagrid/decorators/thumbnail-view',[],function() {
          */
         toggleItemSelected: function(id) {
             if (this.datagrid.itemIsSelected.call(this.datagrid, id) === true) {
-                this.unselectItem(id);
+                this.unselectItem(id, false);
             } else {
-                this.selectItem(id);
+                this.selectItem(id, false);
             }
         },
 
@@ -30072,13 +30070,16 @@ define('husky_components/datagrid/decorators/thumbnail-view',[],function() {
         /**
          * Unselects an item with a given id
          * @param id {Number|String} the id of the item
+         * @param onlyView {Boolean} if true the selection only affects this view and not the data array
          */
-        unselectItem: function(id) {
+        unselectItem: function(id, onlyView) {
             this.sandbox.dom.removeClass(this.$thumbnails[id], constants.selectedClass);
             if (this.sandbox.dom.is(this.sandbox.dom.find('input[type="checkbox"]', this.$thumbnails[id]), ':checked')) {
                 this.sandbox.dom.prop(this.sandbox.dom.find('input[type="checkbox"]', this.$thumbnails[id]), 'checked', false);
             }
-            this.datagrid.setItemUnselected.call(this.datagrid, id);
+            if (onlyView !== true) {
+                this.datagrid.setItemUnselected.call(this.datagrid, id);
+            }
         },
 
         /**
@@ -30118,8 +30119,9 @@ define('husky_components/datagrid/decorators/thumbnail-view',[],function() {
          */
         unselectAll: function() {
             this.sandbox.util.each(this.$thumbnails, function(id) {
-                this.unselectItem(id);
+                this.unselectItem(id, true);
             }.bind(this));
+            this.datagrid.deselectAllItems.call(this.datagrid);
         }
     };
 });
