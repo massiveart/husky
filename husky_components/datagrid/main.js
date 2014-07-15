b810c141c6a55d38827fa23114eb8183d9c8f49b
marcelmoosbrugger <marcel.moosbrugger@massiveart.com>
2014-07-15 10:46:48 +0200

    bugfix

--- husky_components/datagrid/main.js
+++ husky_components/datagrid/main.js
@@ -559,9 +559,9 @@
                         }
                         // push the constructed matching to the global matchings array
                         this.matchings.push(matchingObject);
-                        this.requestFields.push(matching.id);
+                        this.requestFields.push(matching.name);
                     } else if (matching.name === 'id') {
-                        this.requestFields.push(matching.id);
+                        this.requestFields.push(matching.name);
                     }
                     // always load the id (never ever even think about not loading the id)
                 }.bind(this));
