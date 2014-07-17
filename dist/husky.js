e2bfa527db650d3b902a2a0fc034dd6342252840
marcelmoosbrugger <marcel.moosbrugger@massiveart.com>
2014-07-17 11:03:50 +0200

    automatically added build files

--- dist/husky.js
+++ dist/husky.js
@@ -28924,7 +28924,7 @@ define('husky_components/datagrid/decorators/table-view',[],function() {
                     } else if (column.minWidth) {
                         minWidth = column.minWidth;
                     } else {
-                        minWidth = getTextWidth.call(this, column.content, [], isSortable);
+                        minWidth = getTextWidth.call(this, column.content, [], column.sortable);
                         minWidth = (minWidth > this.datagrid.getNumberAndUnit(this.options.columnMinWidth).number) ? minWidth + 'px' : this.options.columnMinWidth;
 
                     }
