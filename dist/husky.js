307c1d22d90c4fbad044b91693af5bed6ba4c039
turbo-ele <elias.hiller@gmx.at>
2014-08-11 12:06:47 +0200

    automatically added build files

--- dist/husky.js
+++ dist/husky.js
@@ -46569,6 +46569,10 @@ define("datepicker-zh-TW", function(){});
               $(selector).empty();
             };
 
+            app.core.dom.replaceWith = function(selector, newContent) {
+              $(selector).replaceWith(newContent);
+            };
+
             /**
              * Awesome visible method. Returns false if any part of a given element is not visible
              * Method is copied and slightly adapted from https://github.com/teamdf/jquery-visible/
