77698d27ba5856ed3cbea2304f066bccd40797f8
Wachter Johannes <johannes.wachter@massiveart.at>
2015-02-16 09:29:58 +0100

    build new version

--- dist/husky.js
+++ dist/husky.js
@@ -42868,12 +42868,24 @@ define('__component__$input@husky',[], function() {
     });
 })();
 
+/**
+ * This file is part of Husky frontend development framework.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ *
+ * @module husky/components/collection-navigation
+ */
+
+
 (function() {
 
     
 
     define('husky_extensions/cache-factory',[], {
-        name: 'history',
+        name: 'cacheFactory',
 
         initialize: function(app) {
             var caches = {};
