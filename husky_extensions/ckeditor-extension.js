b79554aa72015071ee416ba9344fd8409abd73e6
Alexander Schranz <alexander.schranz@massiveart.com>
2015-05-26 10:46:27 +0200

    disallow iframes and script in default

--- husky_extensions/ckeditor-extension.js
+++ husky_extensions/ckeditor-extension.js
@@ -34,7 +34,7 @@
                 removePlugins: 'elementspath,magicline',
                 removeDialogTabs: 'image:advanced;link:advanced',
                 extraPlugins: 'justify,format,sourcearea,link,table,pastefromword,autogrow',
-                extraAllowedContent: 'img(*)[*]; span(*)[*]; div(*)[*]; iframe(*)[*]; script(*)[*]',
+                extraAllowedContent: 'img(*)[*]; span(*)[*]; div(*)[*]',
                 resize_enabled: false,
                 enterMode: 'P',
                 uiColor: '#ffffff',
