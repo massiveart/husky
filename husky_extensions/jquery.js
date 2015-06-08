4fb95ce5d36d19069ca1a54d2ec17f6112100345
xandrej <andrej.frelih@inbox.com>
2015-06-08 12:01:24 +0200

    isAlphaNumeric function

--- husky_extensions/jquery.js
+++ husky_extensions/jquery.js
@@ -211,6 +211,20 @@
                 return $.isNumeric(number);
             };
 
+            app.core.dom.isAlphaNumeric = function(str) {
+                  var code, i, len;
+
+                  for (i = 0, len = str.length; i < len; i++) {
+                    code = str.charCodeAt(i);
+                    if (!(code > 47 && code < 58) && // numeric (0-9)
+                        !(code > 64 && code < 91) && // upper alpha (A-Z)
+                        !(code > 96 && code < 123)) { // lower alpha (a-z)
+                      return false;
+                    }
+                  }
+                  return true;
+            };
+
             app.core.dom.data = function(selector, key, value) {
                 if (!!value || value === '') {
                     return $(selector).data(key, value);
