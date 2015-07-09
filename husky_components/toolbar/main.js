2518cbfbe6b21ffedd6f4562e62e9f658837150a
Michael Zangerle <michael.zangerle@massiveart.com>
2015-07-09 13:55:32 +0200

    fixed issue when divider is first element

--- husky_components/toolbar/main.js
+++ husky_components/toolbar/main.js
@@ -611,6 +611,11 @@ define(function() {
             this.sandbox.util.foreach(parent.items, function(item) {
 
                 if (item.divider) {
+
+                    // prevent first item divider
+                    if(this.items[parent.id].items.length === 0) {
+                        return
+                    }
                     this.sandbox.dom.append($list, '<li class="divider"></li>');
                     return;
                 }
