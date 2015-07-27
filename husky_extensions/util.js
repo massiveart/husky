5b97bdc31b36f442b23a8159436574edb82a5440
Michael Zangerle <michael.zangerle@massiveart.com>
2015-07-27 08:12:57 +0200

    added emptly line

--- husky_extensions/util.js
+++ husky_extensions/util.js
@@ -206,6 +206,7 @@ define(function() {
                 name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
                 var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
                     results = regex.exec(url);
+                    
                 return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
             };
 
