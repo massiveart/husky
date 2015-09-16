dc14a3577be2d28198e34bd4a32051ab3f05a1b0
Niklas Natter <niklas.natter@gmail.com>
2015-09-16 11:58:39 +0200

    implemented infinite-scroll-pagination as datagrid decorator

---
+++ husky_components/datagrid/decorators/infinite-scroll-pagination.js
@@ -0,0 +1,175 @@
+/**
+ * @class InfiniteScrollPagination (Datagrid Decorator)
+ * @constructor
+ *
+ * @param {Object} [paginationOptions] Configuration object
+ * @param {Number} [options.limit] Data records per page
+ * @param {Number} [options.scrollContainer] CSS selector of the element which holds the datagrid items and is scrollable
+ *
+ * @param {Function} [initialize] function which gets called once at the start of the view
+ * @param {Function} [render] function to render data
+ * @param {Function} [destroy] function to destroy the pagination and unbind events
+ * @param {Function} [getHeight] function which returns the height of the pagination
+ */
+define(function() {
+
+    'use strict';
+
+    var defaults = {
+            scrollContainer: '.page',
+            limit: 20
+        },
+
+        constants = {
+            paginationClass: 'pagination-wrapper infinite',
+            paginationElementClass: 'pagination',
+            loaderClass: 'pagination-loader'
+        },
+
+        /**
+         * Templates used by this class
+         */
+        templates = {
+            paginationContainer: [
+                '<div class="' + constants.paginationClass + '">',
+                '   <div class="' + constants.loaderClass + '"/>',
+                '</div>'
+            ].join('')
+        };
+
+    return {
+
+        /**
+         * Initializes the pagination
+         * @param {Object} context The context of the datagrid
+         * @param {Object} options The options used by this pagination
+         */
+        initialize: function(context, options) {
+            // context of the datagrid-component
+            this.datagrid = context;
+
+            // make sandbox available in this-context
+            this.sandbox = this.datagrid.sandbox;
+
+            // merge defaults with pagination options
+            this.options = this.sandbox.util.extend(true, {}, defaults, options);
+
+            this.setVariables();
+        },
+
+        /**
+         * Sets the classes properties
+         */
+        setVariables: function() {
+            this.$el = null;
+            this.$paginationContainer = null;
+            this.$loader = null;
+            this.data = null;
+        },
+
+        /**
+         * Returns the total height of the pagination
+         * @returns {number}
+         */
+        getHeight: function() {
+            return this.sandbox.dom.outerHeight(this.$paginationContainer, true);
+        },
+
+        /**
+         * Renders the pagination
+         */
+        render: function(data, $container) {
+            this.$el = $container;
+            this.data = data;
+
+            this.$paginationContainer = this.sandbox.dom.createElement(templates.paginationContainer);
+            this.$loader = $(this.$paginationContainer).find('.' + constants.loaderClass);
+            this.sandbox.dom.append(this.$el, this.$paginationContainer);
+
+            this.startLoader();
+            this.bindInfiniteScroll();
+        },
+
+        /**
+         * Starts a small loader
+         */
+        startLoader: function() {
+            this.sandbox.start([
+                {
+                    name: 'loader@husky',
+                    options: {
+                        el: this.$loader,
+                        size: '40px',
+                        color: '#cccccc'
+                    }
+                }
+            ]);
+            this.$loader.hide();
+        },
+
+        /**
+         * Returns the pagination page size
+         * @returns {Number} current limit
+         */
+        getLimit: function() {
+            return this.options.limit;
+        },
+
+        /**
+         * Destroys the pagination
+         */
+        destroy: function() {
+            // stop components
+            this.sandbox.stop(this.sandbox.dom.find('*', this.$paginationContainer));
+            // unbind events
+            this.sandbox.dom.unbind(this.sandbox.dom.find('*', this.$paginationContainer));
+            this.sandbox.infiniteScroll.destroy(this.options.scrollContainer);
+            // remove container
+            this.sandbox.dom.remove(this.$paginationContainer);
+        },
+
+        /**
+         * Binds dom related events
+         */
+        bindInfiniteScroll: function() {
+            this.sandbox.infiniteScroll.initialize(this.options.scrollContainer, this.appendNextPage.bind(this), 0);
+        },
+
+        appendNextPage: function() {
+            var promise = $.Deferred();
+
+            if (!!this.datagrid.data.links && !!this.datagrid.data.links.next) {
+                this.$loader.show();
+
+                this.sandbox.util.load(this.datagrid.data.links.next.href).then(function(data) {
+                    this.updateDatagrid(data);
+
+                    this.$loader.hide();
+                    promise.resolve();
+                }.bind(this))
+            } else {
+                promise.resolve();
+            }
+
+            return promise;
+        },
+
+        updateDatagrid: function(data) {
+            this.datagrid.data.links = this.datagrid.parseLinks(data._links);
+            this.datagrid.data.total = data.total;
+            this.datagrid.data.page = data.page;
+            this.datagrid.data.pages = data.pages;
+            this.datagrid.data.limit = data.limit;
+
+            this.addRecordsToDatagrid(data._embedded[this.datagrid.options.resultKey]);
+        },
+        addRecordsToDatagrid: function(records) {
+            this.sandbox.util.foreach(records, function(record) {
+                if (!!record.id) {
+                    this.datagrid.data.embedded.push(record);
+                    this.datagrid.gridViews[this.datagrid.viewId].addRecord(record);
+                }
+            }.bind(this));
+        }
+    };
+});
