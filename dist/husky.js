db277125369c825f3d04113c3de2c92f728fe158
Johannes Wachter <johannes.wachter@massiveart.at>
2015-09-21 11:33:45 +0200

    added slide-to event for overlay

--- dist/husky.js
+++ dist/husky.js
@@ -28491,6 +28491,7 @@ define('husky',[
         app.use('./husky_extensions/colorpicker');
         app.use('./husky_extensions/datepicker');
         app.use('./husky_extensions/itembox');
+        app.use('./husky_extensions/itemgrid');
         app.use('./husky_extensions/cache-factory');
         app.use('./husky_extensions/infinite-scroll');
 
@@ -32685,6 +32686,14 @@ define('husky_components/datagrid/decorators/dropdown-pagination',[],function() 
             },
 
             /**
+             * sets new data to datagrid
+             * @event husky.datagrid.data.set
+             */
+            DATA_SET = function() {
+                return this.createEventName('data.set');
+            },
+
+            /**
              * triggers husky.datagrid.items.selected event, which returns all selected item ids
              * @event husky.datagrid.items.get-selected
              * @param  {Function} callback function receives array of selected items
@@ -33107,6 +33116,26 @@ define('husky_components/datagrid/decorators/dropdown-pagination',[],function() 
             },
 
             /**
+             * Changes data in grid from array
+             * @param data array
+             */
+            setData: function(data) {
+                this.destroy();
+
+                if (!!data.resultKey && !!data[data.resultKey]) {
+                    this.data.embedded = data[data.resultKey];
+                } else {
+                    this.data.embedded = data;
+                }
+
+                if (!!this.paginations[this.paginationId]) {
+                    this.paginations[this.paginationId].render(this.data, this.$element);
+                }
+
+                this.render();
+            },
+
+            /**
              * Parses the sorting params from the url
              * @param url
              */
@@ -33518,6 +33547,7 @@ define('husky_components/datagrid/decorators/dropdown-pagination',[],function() 
                 }
 
                 this.sandbox.on(UPDATE.call(this), this.updateGrid.bind(this));
+                this.sandbox.on(DATA_SET.call(this), this.setData.bind(this));
                 this.sandbox.on(DATA_GET.call(this), this.provideData.bind(this));
                 this.sandbox.on(DATA_SEARCH.call(this), this.searchGrid.bind(this));
                 this.sandbox.on(URL_UPDATE.call(this), this.updateUrl.bind(this));
@@ -41838,6 +41868,15 @@ define('__component__$overlay@husky',[], function() {
             return createEventName.call(this, 'slide-right');
         },
 
+        /**
+         * slide to
+         * @param {Number} slide number
+         * @event husky.overlay.<instance-name>.slide-to
+         */
+        SLIDE_TO = function () {
+            return createEventName.call(this, 'slide-to');
+        },
+
         /** returns normalized event names */
         createEventName = function(postFix) {
             return eventNamespace + (this.options.instanceName ? this.options.instanceName + '.' : '') + postFix;
@@ -42019,6 +42058,7 @@ define('__component__$overlay@husky',[], function() {
         bindCustomEvents: function() {
             this.sandbox.on(SLIDE_LEFT.call(this), this.slideLeft.bind(this));
             this.sandbox.on(SLIDE_RIGHT.call(this), this.slideRight.bind(this));
+            this.sandbox.on(SLIDE_TO.call(this), this.slideEvent.bind(this));
         },
 
         /**
@@ -42040,6 +42080,22 @@ define('__component__$overlay@husky',[], function() {
             if (this.activeSlide >= this.slides.length) {
                 this.activeSlide = 0;
             }
+            this.slideTo(this.activeSlide);
+        },
+
+        /**
+         * slide to given slide number
+         *
+         * @param {Number} slide
+         */
+        slideEvent: function (slide) {
+            if (slide < 0 || slide >= this.slides.length) {
+                this.sandbox.logger.error('Slide index out bounds');
+
+                return;
+            }
+
+            this.activeSlide = slide;
             this.slideTo(this.activeSlide);
         },
 
@@ -50059,6 +50115,318 @@ define('husky_extensions/itembox',[],function() {
 
         initialize: function(app) {
             app.components.addType('itembox', itembox);
+        }
+    }
+});
+
+/**
+ * This file is part of Husky frontend development framework.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ *
+ */
+
+/**
+ * Introduces functionality used by multiple components, which are displaying some items in a list
+ */
+define('husky_extensions/itemgrid',[],function() {
+
+    'use strict';
+
+    var defaults = {
+            instanceName: null,
+            url: null,
+            eventNamespace: 'husky.itemgrid',
+            idsParameter: 'ids',
+            resultKey: null,
+            idKey: 'id',
+            dataAttribute: '',
+            dataDefault: {},
+            sortable: false,
+            removable: true,
+            translations: {
+                noContentSelected: 'listbox.nocontent-selected',
+                viewAll: 'public.view-all',
+                viewLess: 'public.view-less',
+                of: 'public.of',
+                visible: 'public.visible',
+                elementsSelected: 'public.elements-selected'
+            }
+        },
+
+        constants = {
+            noContentClass: 'no-content',
+            addIcon: 'fa-plus-circle',
+            deleteIcon: 'fa-trash-o'
+        },
+
+        /**
+         * returns the normalized event names
+         * @param eventName {string} The name of the concrete event without prefix
+         * @returns {string} Returns the prefixed event name
+         */
+        createEventName = function(eventName) {
+            return [
+                this.options.eventNamespace,
+                '.',
+                (this.options.instanceName ? this.options.instanceName + '.' : ''),
+                eventName
+            ].join('');
+        },
+
+        templates = {
+            skeleton: function() {
+                return [
+                    '<div class="listgrid" id="', this.ids.container, '">',
+                    '    <div class="header">',
+                    '        <span class="', constants.addIcon ,' icon left action add-button" id="', this.ids.addButton, '"></span>',
+                    '        <span class="', constants.deleteIcon ,' icon left action delete-button" id="', this.ids.deleteButton, '"></span>',
+                    '    </div>',
+                    '    <div class="content" id="', this.ids.content, '"></div>',
+                    '</div>'
+                ].join('');
+            }
+        },
+
+        bindCustomEvents = function() {
+            this.sandbox.on(this.DATA_CHANGED(), this.changeData.bind(this));
+
+            this.sandbox.on(this.DELETE_BUTTON_CLICKED(), function() {
+                this.sandbox.emit('husky.datagrid.' + this.options.instanceName + '.items.get-selected', function(ids) {
+                    deleteItems.call(this, ids);
+                }.bind(this));
+            }.bind(this));
+
+            this.sandbox.on(this.ITEM_ADD(), function(item) {
+                addItem.call(this, item);
+            }.bind(this));
+
+            this.sandbox.on(this.ITEM_SAVE(), function(item) {
+                editItem.call(this, item);
+            }.bind(this));
+        },
+
+        bindDomEvents = function() {
+            // click on the add button
+            this.sandbox.dom.on(this.$addButton, 'click', function() {
+                this.sandbox.emit(this.ADD_BUTTON_CLICKED());
+            }.bind(this));
+
+            // click on the config button
+            this.sandbox.dom.on(this.$deleteButton, 'click', function() {
+                this.sandbox.emit(this.DELETE_BUTTON_CLICKED());
+            }.bind(this));
+        },
+
+        addItem = function(newItem) {
+            var data = this.getData();
+
+            if (this.sandbox.util.typeOf(data) !== 'array' || this.sandbox.util.typeOf(data[0]) !== 'object') {
+                data = [];
+            }
+
+            data.push(newItem);
+            this.setData(data);
+        },
+
+        editItem = function(editedItem) {
+            var newData = [], data = this.getData();
+            data.forEach(function(item) {
+                if (item.id === editedItem.id) {
+                    newData.push(editedItem);
+                } else {
+                    newData.push(item);
+                }
+            }.bind(this));
+
+            this.setData(newData);
+        },
+
+        deleteItems = function(ids) {
+            this.sandbox.sulu.showDeleteDialog(function(wasConfirmed) {
+                if (wasConfirmed) {
+                    var newData = [], data = this.getData();
+                    data.forEach(function(item) {
+                        if (ids.indexOf(item.id) > -1) {
+                            this.sandbox.emit('husky.datagrid.' + this.options.instanceName + '.record.remove', item.id);
+                            this.sandbox.emit('sulu.header.toolbar.item.enable', 'save', false);
+                        } else {
+                            newData.push(item);
+                        }
+                    }.bind(this));
+
+                    this.setData(newData);
+                }
+            }.bind(this));
+        },
+
+        itemgrid = {
+            /**
+             * raised when the data changed and the list should be reloaded
+             * @event husky.itembox.data-changed
+             * @return {string}
+             */
+            DATA_CHANGED: function() {
+                return createEventName.call(this, 'data-changed');
+            },
+
+            /**
+             * raised when the add button was clicked
+             * @event husky.itembox.add-button-clicked
+             * @return {string}
+             */
+            ADD_BUTTON_CLICKED: function() {
+                return createEventName.call(this, 'add-button-clicked');
+            },
+
+            /**
+             * raised when the config button was clicked
+             * @event husky.itembox.config-button-clicked
+             * @return {string}
+             */
+            DELETE_BUTTON_CLICKED: function() {
+                return createEventName.call(this, 'delete-button-clicked');
+            },
+
+            /**
+             * raised when a new item should be persisted
+             * @event husky.itembox.config-button-clicked
+             * @return {string}
+             */
+            ITEM_ADD: function() {
+                return createEventName.call(this, 'add-item');
+            },
+
+            /**
+             * raised when an existing item should be persisted
+             * @event husky.itembox.config-button-clicked
+             * @return {string}
+             */
+            ITEM_SAVE: function() {
+                return createEventName.call(this, 'save-item');
+            },
+            /**
+             * render the itembox
+             */
+            render: function() {
+                this.options = this.sandbox.util.extend(true, {}, defaults, this.options);
+
+                var data = this.getData();
+
+                this.viewAll = true;
+
+                this.ids = {
+                    container: 'listbox-' + this.options.instanceName + '-container',
+                    addButton: 'listbox-' + this.options.instanceName + '-add',
+                    deleteButton: 'listbox-' + this.options.instanceName + '-delete',
+                    content: 'listbox-' + this.options.instanceName + '-content'
+                };
+
+                this.sandbox.dom.html(this.$el, templates.skeleton.call(this));
+
+                this.$container = this.sandbox.dom.find(this.getId('container'), this.$el);
+                this.$addButton = this.sandbox.dom.find(this.getId('addButton'), this.$el);
+                this.$deleteButton = this.sandbox.dom.find(this.getId('deleteButton'), this.$el);
+                this.$content = this.sandbox.dom.find(this.getId('content'), this.$el);
+
+                bindCustomEvents.call(this);
+                bindDomEvents.call(this);
+
+                this.renderContent(data);
+            },
+
+            /**
+             * Returns the data currently stored in this component
+             * @param deepCopy {boolean} True if deep cop should be returned, otherwise false
+             * @returns {object}
+             */
+            getData: function() {
+                return this.sandbox.util.deepCopy(this.sandbox.dom.data(this.$el, this.options.dataAttribute));
+            },
+
+            /**
+             * Throws a data-changed event if the data actually has changed
+             * @param data {object} The data to set
+             * @param reload {boolean} True if the itembox list should be reloaded afterwards
+             */
+            setData: function(data, reload) {
+                this.sandbox.emit(this.DATA_CHANGED(), data, this.$el, reload);
+            },
+
+            /**
+             * Event handler for the changed data event, sets data to element and reloads the list if specified
+             * @param data {object} The data to set
+             * @param $el {object} The element to which the data should be bound
+             * @param reload {boolean} True if the list should be reloaded, otherwise false
+             */
+            changeData: function (data) {
+                this.updateOrder(data);
+
+                this.sandbox.emit('husky.datagrid.' + this.options.instanceName + '.data.set', data);
+
+                this.sandbox.dom.data(this.$el, this.options.dataAttribute, data);
+
+                this.sandbox.emit('sulu.header.toolbar.item.enable', 'save', false);
+            },
+
+            /**
+             * Gets data and returns them in sorted order
+             * @param data {object} The data, which should be set into the grid
+             */
+            updateOrder: function(data) {
+                throw new Error('"updateOrder" not implemented');
+            },
+
+            /**
+             * Returns the selector for the given id
+             * @param type {string} The type of the element, for which the id should be returned
+             * @returns {string} The id of the element
+             */
+            getId: function(type) {
+                return ['#', this.ids[type]].join('');
+            },
+
+            /**
+             * Renders the data into the list
+             * @param data {object} The data to render
+             */
+            renderContent: function(data) {
+                var template = [
+                        '<div id="', this.options.instanceName, '-list-toolbar-container"></div>',
+                        '<div id="itemgrid-list-', this.options.instanceName, '"></div>'
+                    ].join('');
+
+                this.sandbox.dom.html(this.$content, template);
+
+                //start list-toolbar and datagrid
+                this.sandbox.start([
+                    {
+                        name: 'datagrid@husky',
+                        options: {
+                            el: '#itemgrid-list-' + this.options.instanceName,
+                            instanceName: this.options.instanceName,
+                            view: 'table',
+                            data: data,
+                            matchings: this.options.fieldList,
+                            viewOptions: {
+                                table: {
+                                    noItemsText: 'public.empty-list'
+                                }
+                            }
+                        }
+                    }
+                ]);
+            }
+        };
+
+    return {
+        name: 'itemgrid',
+
+        initialize: function(app) {
+            app.components.addType('itemgrid', itemgrid);
         }
     }
 });
--- husky_components/overlay/main.js
+++ husky_components/overlay/main.js
@@ -312,6 +312,15 @@ define([], function() {
             return createEventName.call(this, 'slide-right');
         },
 
+        /**
+         * slide to
+         * @param {Number} slide number
+         * @event husky.overlay.<instance-name>.slide-to
+         */
+        SLIDE_TO = function () {
+            return createEventName.call(this, 'slide-to');
+        },
+
         /** returns normalized event names */
         createEventName = function(postFix) {
             return eventNamespace + (this.options.instanceName ? this.options.instanceName + '.' : '') + postFix;
@@ -493,6 +502,7 @@ define([], function() {
         bindCustomEvents: function() {
             this.sandbox.on(SLIDE_LEFT.call(this), this.slideLeft.bind(this));
             this.sandbox.on(SLIDE_RIGHT.call(this), this.slideRight.bind(this));
+            this.sandbox.on(SLIDE_TO.call(this), this.slideEvent.bind(this));
         },
 
         /**
@@ -514,6 +524,22 @@ define([], function() {
             if (this.activeSlide >= this.slides.length) {
                 this.activeSlide = 0;
             }
+            this.slideTo(this.activeSlide);
+        },
+
+        /**
+         * slide to given slide number
+         *
+         * @param {Number} slide
+         */
+        slideEvent: function (slide) {
+            if (slide < 0 || slide >= this.slides.length) {
+                this.sandbox.logger.error('Slide index out bounds');
+
+                return;
+            }
+
+            this.activeSlide = slide;
             this.slideTo(this.activeSlide);
         },
 
