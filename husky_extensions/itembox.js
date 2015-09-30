f9c9ee487ae2510915cc6797af3f3f8861caa5ed
Johannes Wachter <johannes.wachter@massiveart.at>
2015-09-30 14:33:45 +0200

    fixed empty request condition

--- husky_extensions/itembox.js
+++ husky_extensions/itembox.js
@@ -327,7 +327,7 @@ define(function() {
                 // reset items visible when new content is loaded
                 this.viewAll = false;
 
-                if (!!data && data.length > 0) {
+                if (!!data && (!data.length || data.length > 0)) {
                     this.sandbox.util.load(this.getUrl(data))
                         .then(function(data) {
                             this.$container.removeClass(constants.isLoadingClass);
